87d8883561ba12783a540322872bb6ab8b7d0104
ZP <zairaperez01@gmail.com>
2021-12-29 11:49:21 -0600

    handling errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,10 +1,26 @@
+var repoNameEl = document.querySelector("#repo-name");
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
 
+var getRepoName = function() {
+  // grab repo name from url query strin
+  var queryString = document.location.search;
+  var repoName = queryString.split("=")[1];
+ 
+  if (repoName){
+    // display repo name on the page
+    repoNameEl.textContent = repoName;
+    
+    getRepoIssues(repoName);
 
+  } else {
+    // if no repo was given, redirect to the homepage
+    document.location.replace("./index.html");
+  }
+};
 var getRepoIssues = function(repo) {
-    console.log(repo);
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
+    // make a get request to url
     fetch(apiUrl).then(function(response) {
         // request was successful
         if (response.ok) {
@@ -18,7 +34,8 @@ var getRepoIssues = function(repo) {
           });
         }
         else {
-          alert("There was a problem with your request!");
+          // if not successful, redirect to homepage
+          document.location.replace("./index.html");
         }
       });
 
@@ -71,4 +88,4 @@ issueContainerEl.appendChild(issueEl);
         limitWarningEl.appendChild(linkEl);
     };
   
-  getRepoIssues("facebook/react");
+  getRepoName();
